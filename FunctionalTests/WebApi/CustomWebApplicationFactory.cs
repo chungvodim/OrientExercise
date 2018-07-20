adaa5da0fb775e0b0400e987d102cfaf486068e0
chungvodim <ba198647@gmail.com>
2018-07-20 12:23:22 +0700

    add FunctionalTests.csproj

---
+++ FunctionalTests/WebApi/CustomWebApplicationFactory.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApp;
+using Infrastructure.Data;
+
+namespace FunctionalTests.WebApi
+{
+    public class CustomWebApplicationFactory<TStartup>
+    : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Create a new service provider.
+
+                //var serviceProvider = new ServiceCollection()
+                //    .AddEntityFrameworkInMemoryDatabase()
+                //    .BuildServiceProvider();
+
+                //// Add a database context (ApplicationDbContext) using an in-memory 
+                //// database for testing.
+                //services.AddDbContext<MyDbContext>(options =>
+                //{
+                //    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                //    options.UseInternalServiceProvider(serviceProvider);
+                //});
+
+
+                //// Build the service provider.
+                //var sp = services.BuildServiceProvider();
+
+                //// Create a scope to obtain a reference to the database
+                //// context (ApplicationDbContext).
+                //using (var scope = sp.CreateScope())
+                //{
+                //    var scopedServices = scope.ServiceProvider;
+                //    var db = scopedServices.GetRequiredService<MyDbContext>();
+                //    var loggerFactory = scopedServices.GetRequiredService<ILoggerFactory>();
+
+                //    var logger = scopedServices
+                //        .GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                //    // Ensure the database is created.
+                //    db.Database.EnsureCreated();
+
+                //    try
+                //    {
+                //        // Seed the database with test data.
+                //        MyDbContextSeed.SeedAsync(db, loggerFactory).Wait();
+                //    }
+                //    catch (Exception ex)
+                //    {
+                //        logger.LogError(ex, $"An error occurred seeding the " +
+                //            "database with test messages. Error: {ex.Message}");
+                //    }
+                //}
+            });
+        }
+    }
+}
