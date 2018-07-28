eb9d2f0ecb119f9e498de190a6ffb454f89763bb
chungvodim <ba198647@gmail.com>
2018-07-28 23:10:29 +0700

    FunctionalTests

---
+++ FunctionalTests/WebApi/Controllers/ProductControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using ApplicationCore.DTO;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp;
+using Xunit;
+
+namespace FunctionalTests.WebApi.Controllers
+{
+    public class ProductControllerTest : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        public ProductControllerTest(CustomWebApplicationFactory<Startup> factory)
+        {
+            Client = factory.CreateClient();
+        }
+
+        public HttpClient Client { get; }
+
+        [Fact]
+        public async Task ReturnsFirst10CatalogItems()
+        {
+            var response = await Client.GetAsync("/Product/GetProductsByPackageID?PackageID=1");
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var model = JsonConvert.DeserializeObject<IEnumerable<ProductDTO>>(stringResponse);
+
+            Assert.Equal(2, model.Count());
+        }
+    }
+}
--- FunctionalTests/WebApi/CustomWebApplicationFactory.cs
+++ FunctionalTests/WebApi/CustomWebApplicationFactory.cs
@@ -20,47 +20,47 @@ namespace FunctionalTests.WebApi
             {
                 // Create a new service provider.
 
-                //var serviceProvider = new ServiceCollection()
-                //    .AddEntityFrameworkInMemoryDatabase()
-                //    .BuildServiceProvider();
+                var serviceProvider = new ServiceCollection()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
 
-                //// Add a database context (ApplicationDbContext) using an in-memory 
-                //// database for testing.
-                //services.AddDbContext<MyDbContext>(options =>
-                //{
-                //    options.UseInMemoryDatabase("InMemoryDbForTesting");
-                //    options.UseInternalServiceProvider(serviceProvider);
-                //});
+                // Add a database context (ApplicationDbContext) using an in-memory 
+                // database for testing.
+                services.AddDbContext<MyDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                    options.UseInternalServiceProvider(serviceProvider);
+                });
 
 
-                //// Build the service provider.
-                //var sp = services.BuildServiceProvider();
+                // Build the service provider.
+                var sp = services.BuildServiceProvider();
 
-                //// Create a scope to obtain a reference to the database
-                //// context (ApplicationDbContext).
-                //using (var scope = sp.CreateScope())
-                //{
-                //    var scopedServices = scope.ServiceProvider;
-                //    var db = scopedServices.GetRequiredService<MyDbContext>();
-                //    var loggerFactory = scopedServices.GetRequiredService<ILoggerFactory>();
+                // Create a scope to obtain a reference to the database
+                // context (ApplicationDbContext).
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<MyDbContext>();
+                    var loggerFactory = scopedServices.GetRequiredService<ILoggerFactory>();
 
-                //    var logger = scopedServices
-                //        .GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+                    var logger = scopedServices
+                        .GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
 
-                //    // Ensure the database is created.
-                //    db.Database.EnsureCreated();
+                    // Ensure the database is created.
+                    db.Database.EnsureCreated();
 
-                //    try
-                //    {
-                //        // Seed the database with test data.
-                //        MyDbContextSeed.SeedAsync(db, loggerFactory).Wait();
-                //    }
-                //    catch (Exception ex)
-                //    {
-                //        logger.LogError(ex, $"An error occurred seeding the " +
-                //            "database with test messages. Error: {ex.Message}");
-                //    }
-                //}
+                    try
+                    {
+                        // Seed the database with test data.
+                        MyDbContextSeed.SeedAsync(db, loggerFactory).Wait();
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, $"An error occurred seeding the " +
+                            "database with test messages. Error: {ex.Message}");
+                    }
+                }
             });
         }
     }
